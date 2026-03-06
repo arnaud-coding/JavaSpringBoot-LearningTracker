110d5b30d390d0d8298e646dd823b6adf4f56787
arnaud-coding <arnaudberthollet@hotmail.fr>
2026-03-06 13:24:22 +0100

    Initial commit: Project setup with Spring initializr

---
+++ src/test/java/com/cefii/learning/learning_tracker/LearningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefii.learning.learning_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
